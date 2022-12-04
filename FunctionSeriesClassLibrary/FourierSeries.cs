9768c1596d90484f81a62b966169739283af567d
UsikovKonstantin <99312736+UsikovKonstantin@users.noreply.github.com>
2022-12-04 21:08:46 +0300

    Начало класса Фурье, поля, ToString().

--- FunctionSeriesClassLibrary/FourierSeries.cs
+++ FunctionSeriesClassLibrary/FourierSeries.cs
@@ -1,7 +1,144 @@
-﻿namespace FunctionSeriesClassLibrary
+﻿using System.Net.Http.Headers;
+
+namespace FunctionSeriesClassLibrary
 {
+    /// <summary>
+    /// Тип ряда Фурье.
+    /// CosSin - разложение в ряд синусов и косинусов.
+    /// Cos - разложение в ряд косинусов (функция рассматривается как чётная).
+    /// Sin -  разложение в ряд синусов (функция рассматривается как нечётная).
+    /// </summary>
+    public enum FourierSeriesType
+    {
+        CosSin,
+        Cos,
+        Sin
+    }
+
+    /// <summary>
+    /// Класс "Ряд Фурье".
+    /// </summary>
     public class FourierSeries
     {
+        private int n = 0;  // количество членов
+        private double period = 0;  // период функции
+        private string function = "";  // функция, по которой строится ряд
+        private double[] a = new double[0];  // коэффициенты при косинусах 
+        private double[] b = new double[0];  // коэффициенты при синусах
+        private FourierSeriesType type = FourierSeriesType.CosSin;  // тип ряда
 
+        // Свойства
+        public int N
+        {
+            get { return n; }
+        }
+        public string Function
+        {
+            get { return function; }
+        }
+        public double Period
+        {
+            get { return period; }
+        }
+        public double[] A
+        {
+            get { return a; }
+        }
+        public double[] B
+        {
+            get { return b; }
+        }
+        public FourierSeriesType Type
+        {
+            get { return type; }
+        }
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="n"> количество членов </param>
+        /// <param name="period"> период функции </param>
+        /// <param name="function"> функция, по которой строится ряд </param>
+        /// <param name="type"> тип ряда </param>
+        public FourierSeries(int n, double period, string function, FourierSeriesType type)
+        {
+            this.n = n;
+            this.period = period;
+            this.function = function;
+            this.type = type;
+            a = new double[n + 1];
+            b = new double[n + 1];
+        }
+
+        /// <summary>
+        /// Переопределение метода ToString().
+        /// </summary>
+        /// <returns> строковое представление ряда Фурье</returns>
+        public override string ToString()
+        {
+            int i = 0;
+            string res = FindFirst(ref i);
+            for (int j = i + 1; j <= n; j++)
+            {
+                if (a[j] != 0)
+                    if (a[j] == 1)
+                        res += " + Cos(" + (j == 1 ? "" : j) + "x)";
+                    else if (a[j] == -1)
+                        res += " - Cos(" + (j == 1 ? "" : j) + "x)";
+                    else
+                        res += (a[j] > 0 ? " + " : " - ") + Math.Abs(a[j]) + "*" + "Cos(" + (j == 1 ? "" : j) + "x)";
+                if (b[j] != 0)
+                    if (b[j] == 1)
+                        res += " + Sin(" + (j == 1 ? "" : j) + "x)";
+                    else if (b[j] == -1)
+                        res += " - Sin(" + (j == 1 ? "" : j) + "x)";
+                    else
+                        res += (b[j] > 0 ? " + " : " - ") + Math.Abs(b[j]) + "*" + "Sin(" + (j == 1 ? "" : j) + "x)";
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// Поиск первого ненулевого члена в массиве коэффициентов.
+        /// </summary>
+        /// <param name="i"> индекс </param>
+        /// <returns> первый ненулевой член ряда </returns>
+        private string FindFirst(ref int i)
+        {
+            string res = "";
+            if (a[0] != 0)
+                return (a[0] / 2).ToString();
+            for (i = 1; i <= n; i++)
+            {
+                if (a[i] != 0)
+                {
+                    if (a[i] == 1)
+                        res = "Cos(" + (i == 1 ? "" : i) + "x)";
+                    else if (a[i] == -1)
+                        res = "-Cos(" + (i == 1 ? "" : i) + "x)";
+                    else
+                        res = a[i] + "*" + "Cos(" + (i == 1 ? "" : i) + "x)";
+                    if (b[i] != 0)
+                        if (b[i] == 1)
+                            res += " + Sin(" + (i == 1 ? "" : i) + "x)";
+                        else if (b[i] == -1)
+                            res += " - Sin(" + (i == 1 ? "" : i) + "x)";
+                        else
+                            res += " + " + b[i] + "*" + "Sin(" + (i == 1 ? "" : i) + "x)";
+                    return res;
+                }
+                if (b[i] != 0)
+                {
+                    if (b[i] == 1)
+                        res = "Sin(" + (i == 1 ? "" : i) + "x)";
+                    else if (b[i] == -1)
+                        res = "-Sin(" + (i == 1 ? "" : i) + "x)";
+                    else
+                        res = b[i] + "*" + "Sin(" + (i == 1 ? "" : i) + "x)";
+                    return res;
+                }
+            }
+            return res;
+        }
     }
 }
---
+++ FunctionSeriesClassLibrary/Interpreter.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctionSeriesClassLibrary
+{
+    public static class Interpreter
+    {
+        // Метод возвращает обратную польскую запись
+        public static string GetPolishExpression(string input)
+        {
+            string output = "";
+            Stack<string> operStack = new Stack<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (IsDelimeter(input[i])) continue;
+
+                if (IsOperator(input[i]))
+                {
+                    if (input[i] == '(')
+                        operStack.Push(input[i].ToString());
+                    else if (input[i] == ')')
+                    {
+                        // Выписываем все операторы до открывающей скобки в строку
+                        string s = operStack.Pop();
+
+                        while (s != "(")
+                        {
+                            output += s.ToString() + ' ';
+                            s = operStack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        while (operStack.Count > 0 && GetPriority(input[i].ToString()) <= GetPriority(operStack.Peek().ToString()))
+                            output += operStack.Pop().ToString() + " ";
+
+                        operStack.Push(input[i].ToString());
+                    }
+                }
+                else if (IsFunction(i, input))
+                {
+                    HashSet<string> possibleFuncs = new HashSet<string>() { "sin", "cos", "tan", "ctan", "arcsin", "arccos", "arctan", "arcctan", "log", "ln", "sqrt", "√" };
+                    string func = "";
+                    // Читаем до разделителя или оператора, чтобы получить число
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        func += input[i];
+                        i++;
+
+                        if (possibleFuncs.Contains(func)) break;
+                        // Если символ - последний, то выходим из цикла
+                        if (i == input.Length) break;
+                    }
+                    if (!possibleFuncs.Contains(func)) throw new Exception("Выражение введено не верно");
+                    operStack.Push(func);
+
+                    i--;
+                }
+                else if (char.IsDigit(input[i]) || char.IsLetter(input[i]))
+                {
+                    // Читаем до разделителя или оператора, чтобы получить число
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        output += input[i];
+                        i++;
+
+                        // Если символ - последний, то выходим из цикла
+                        if (i == input.Length) break;
+                    }
+
+                    output += ' ';
+                    i--;
+                }
+            }
+
+            // Когда прошли по всем символам, выкидываем из стека все оставшиеся там операторы в строку
+            while (operStack.Count > 0)
+                output += operStack.Pop() + " ";
+
+            return output;
+        }
+
+        public static double SolvePolishExpression(string s, Dictionary<char, double> variables)
+        {
+            Stack<double> values = new Stack<double>();
+            string[] polishNotation = ConvertVarsToNums(s.Trim().Split(' '), variables);
+            foreach (string cur in polishNotation)
+            {
+                if (!IsFunction(0, cur))
+                {
+                    if (cur.Length == 1 && IsOperator(char.Parse(cur)))
+                    {
+                        //  Вычисление значений в зависимости от операции
+                        double result = ComputeOperation(cur, values);
+                        values.Push(result);
+                    }
+                    else values.Push(double.Parse(cur));
+                }
+                else
+                {
+                    //  Вычисление значений в зависимости от операции
+                    double result = ComputeOperation(cur, values);
+                    values.Push(result);
+                }
+            }
+
+            return values.Pop();
+        }
+
+        // Есть ли переменные в выражении
+        public static bool IsVarInPolExpression(string s)
+        {
+            string[] polishNotation = GetPolishExpression(s).Trim().Split(' ');
+
+            for (int i = 0; i < polishNotation.Length; i++)
+            {
+                // Если текущий элемент - переменная
+                bool isConst = polishNotation[i] == "e" || polishNotation[i] == "π";
+                if (polishNotation[i].Length == 1 && char.IsLetter(char.Parse(polishNotation[i])) && !isConst) return true;
+            }
+
+            return false;
+        }
+
+        // Получить все переменные в выражении
+        public static List<char> GetVarsFromExpression(string s)
+        {
+            List<char> res = new List<char>();
+
+            string[] polishNotation = GetPolishExpression(s).Trim().Split(' ');
+            for (int i = 0; i < polishNotation.Length; i++)
+            {
+                // Если текущий элемент - переменная
+                bool isConst = polishNotation[i] == "e" || polishNotation[i] == "π";
+                if (polishNotation[i].Length == 1 && char.IsLetter(char.Parse(polishNotation[i])) && !isConst) res.Add(char.Parse(polishNotation[i]));
+            }
+
+            return res;
+        }
+
+        // Метод возвращает true, если проверяемый символ - пробел
+        private static bool IsDelimeter(char c)
+        {
+            if ((" ".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+
+        // Метод возвращает true, если проверяемый символ - оператор
+        private static bool IsOperator(char с)
+        {
+            if (("+-/÷:*×^()!".IndexOf(с) != -1))
+                return true;
+            return false;
+        }
+
+        // Метод возвращает true, если проверяемый символ - функция
+        private static bool IsFunction(int idx, string s)
+        {
+            bool isFunction = char.IsLetter(s[idx]) && idx <= s.Length - 2 && char.IsLetter(s[idx + 1]);
+            bool isSqrt = '√' == s[idx];
+            if (isFunction || isSqrt)
+                return true;
+            return false;
+        }
+
+        // Метод возвращает приоритет оператора
+        private static int GetPriority(string s)
+        {
+            if (s.Length > 1 || s == "√" || s == "!") return 7;
+            switch (s)
+            {
+                case "(": return 0;
+                case ")": return 1;
+                case "+": return 2;
+                case "-": return 3;
+                case "*": return 4;
+                case "/": return 4;
+                case "^": return 5;
+                default: return 8;
+            }
+        }
+
+        // Выполнение операций
+        private static double ComputeOperation(string oper, Stack<double> values)
+        {
+            if (oper == "+")
+            {
+                double firstVal = values.Pop();
+                double secondVal = values.Pop();
+                return firstVal + secondVal;
+            }
+            else if (oper == "-")
+            {
+                double firstVal = values.Pop();
+                if (values.Count > 0)
+                {
+                    double secondVal = values.Pop();
+                    return secondVal - firstVal;
+                }
+                else
+                {
+                    return -firstVal;
+                }
+            }
+            else if (oper == "*" || oper == "×")
+            {
+                double firstVal = values.Pop();
+                double secondVal = values.Pop();
+                return secondVal * firstVal;
+            }
+            else if (oper == "/" || oper == ":" || oper == "÷")
+            {
+                double firstVal = values.Pop();
+                double secondVal = values.Pop();
+                return secondVal / firstVal;
+            }
+            else if (oper == "^")
+            {
+                double firstVal = values.Pop();
+                double secondVal = values.Pop();
+                return Math.Pow(secondVal, firstVal);
+            }
+            else if (oper == "!")
+            {
+                double firstVal = values.Pop();
+                double res = 1;
+                for (int i = 2; i <= firstVal; i++)
+                {
+                    res *= i;
+                }
+                return res;
+            }
+            else if (oper == "log")
+            {
+                double firstVal = values.Pop();
+                return Math.Log10(firstVal);
+            }
+            else if (oper == "ln")
+            {
+                double firstVal = values.Pop();
+                return Math.Log(firstVal);
+            }
+            else if (oper == "sin")
+            {
+                double firstVal = values.Pop();
+                return Math.Sin(firstVal);
+            }
+            else if (oper == "cos")
+            {
+                double firstVal = values.Pop();
+                return Math.Cos(firstVal);
+            }
+            else if (oper == "tan")
+            {
+                double firstVal = values.Pop();
+                return Math.Tan(firstVal);
+            }
+            else if (oper == "ctan")
+            {
+                double firstVal = values.Pop();
+                return 1 / Math.Tan(firstVal);
+            }
+            else if (oper == "arcsin")
+            {
+                double firstVal = values.Pop();
+                return Math.Asin(firstVal);
+            }
+            else if (oper == "arccos")
+            {
+                double firstVal = values.Pop();
+                return Math.Acos(firstVal);
+            }
+            else if (oper == "arctan")
+            {
+                double firstVal = values.Pop();
+                return Math.Atan(firstVal);
+            }
+            else if (oper == "arcctan")
+            {
+                double firstVal = values.Pop();
+                return 1 / Math.Atan(firstVal);
+            }
+            else if (oper == "sqrt" || oper == "√")
+            {
+                double firstVal = values.Pop();
+                return Math.Sqrt(firstVal);
+            }
+            else
+            {
+                throw new Exception("Несуществующая функция или оператор");
+            }
+        }
+
+        // Заменить переменные на числа
+        private static string[] ConvertVarsToNums(string[] polishNotation, Dictionary<char, double> variables)
+        {
+            for (int i = 0; i < polishNotation.Length; i++)
+            {
+                // Если текущий элемент - переменная
+                if (polishNotation[i].Length == 1 && char.IsLetter(char.Parse(polishNotation[i])))
+                {
+                    if (polishNotation[i] == "π")
+                    {
+                        polishNotation[i] = $"{Math.PI}";
+                    }
+                    else if (polishNotation[i] == "e")
+                    {
+                        polishNotation[i] = $"{Math.Exp(1)}";
+                    }
+                    else
+                    {
+                        // Достаём число для переменной, если его нет выкидывает ошибку
+                        bool hasVar = variables.TryGetValue(char.Parse(polishNotation[i]), out double num);
+                        if (!hasVar) throw new ApplicationException($"Переменной '{polishNotation[i]}' нету");
+                        polishNotation[i] = $"{num}";
+                    }
+                }
+            }
+
+            return polishNotation;
+        }
+    }
+}

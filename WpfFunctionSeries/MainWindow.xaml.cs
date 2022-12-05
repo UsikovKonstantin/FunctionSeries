c05d20b1d005408a6ff2e6f8230d272d886b11c1
Nydus Born <nydusborn@outlook.com>
2022-12-05 11:42:07 +0300

    Добавил кнопки

--- WpfFunctionSeries/MainWindow.xaml.cs
+++ WpfFunctionSeries/MainWindow.xaml.cs
@@ -25,16 +25,42 @@ namespace WpfFunctionSeries
             InitializeComponent();
         }
 
+        private int prev_scroll_value = 5;
         private void Scr_Pow_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            
+            if (!IsInitialized) return;
+            if (Math.Abs(Math.Round(Scr_Pow.Value) - prev_scroll_value) > 0.1)
+            {
+                prev_scroll_value = (int)Math.Round(Scr_Pow.Value);
+                Tx_Terms_Input.Text = prev_scroll_value.ToString();
+                General_Calc(sender, e);
+            }
             
         }
         
         private void General_Calc(object sender, RoutedEventArgs e)
         {
-            
+            if (!IsInitialized) return;
+            checks_terms();
         }
 
+        void checks_terms()
+        {
+            
+        }
+        private void Bt_Help_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!IsInitialized) return;
+        }
+
+        private void Bt_Approx_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!IsInitialized) return;
+        }
+
+        private void Bt_Text_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!IsInitialized) return;
+        }
     }
 }

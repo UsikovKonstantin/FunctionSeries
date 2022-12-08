d08eca50142d92d6156270da18cb7c6f9c4b3fe4
Nydus Born <nydusborn@outlook.com>
2022-12-08 22:55:07 +0300

    Вероятно рабочая версия NUDFT

--- FunctionSeriesConsoleApp/Program.cs
+++ FunctionSeriesConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using FunctionSeriesClassLibrary;
 using System.Diagnostics;
+using System.Numerics;
 
 //FourierSeries fs = new FourierSeries(10, 6.28, "x", FourierSeriesType.Sin);
 //Console.WriteLine(fs);
@@ -12,3 +13,5 @@ using System.Diagnostics;
 string test = "3";
 bool test2 = Interpreter.IsVarInPolExpression(test);
 Console.WriteLine(test2);
+
+Console.WriteLine(new FourierSeries(new List<(double x, double y)>() {(1,1),(2,2)}, 2).Compute(1.4));

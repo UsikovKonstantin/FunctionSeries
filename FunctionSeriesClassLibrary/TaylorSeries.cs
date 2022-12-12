b07c385db524b44531e970638508c65342de183f
Artyom Ivanov <Artyom655@yandex.ru>
2022-12-13 00:34:08 +0300

    добавил часть рядов Тейлора

---
+++ FunctionSeriesClassLibrary/TaylorSeries.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Symbolics;
+using Expr = MathNet.Symbolics.SymbolicExpression;
+
+namespace FunctionSeriesClassLibrary {
+    public class TaylorSeries {
+        /// <summary>
+        /// Заданная функция
+        /// </summary>
+        public string Function { get; }
+        /// <summary>
+        /// x0
+        /// </summary>
+        public double X0 { get; }
+        /// <summary>
+        /// Коэффициенты
+        /// </summary>
+        public double[] Coefs { get; }
+        /// <summary>
+        /// Кол-во коэффициентов в ряде
+        /// </summary>
+        public int N { get; }
+
+
+        public TaylorSeries(string function, double x0, int n) {
+            if (n < 0) throw new Exception("Кол-во коэффициентов в ряду должно быть больше нуля");
+            Function = function;
+            X0 = x0;
+            Coefs = new double[n + 1];
+            N = n;
+            FindCoefs();
+        }
+
+        /// <summary>
+        /// Найти первый коэфициент ряда
+        /// </summary>
+        /// <returns></returns>
+        public double FindFirst() {
+            string pol = Interpreter.GetPolishExpression(Function);
+            double res = Interpreter.SolvePolishExpression(pol, new Dictionary<char, double>() { { 'x', X0 } });
+            return res;
+        }
+
+        /// <summary>
+        /// Найти все коэфициенты ряда
+        /// </summary>
+        public void FindCoefs() {
+            Coefs[0] = FindFirst();
+
+            Expr x = Expr.Variable("x");
+            Expr prevDerivative = new Expr(Infix.Parse(Function).ResultValue);
+            for (int i = 1; i < N + 1; i++) {
+                Expr derivative = prevDerivative.Differentiate(x);
+                string pol = Interpreter.GetPolishExpression(derivative.ToString());
+                Coefs[i] = Interpreter.SolvePolishExpression(pol, new Dictionary<char, double>() { { 'x', X0 } });
+                prevDerivative = derivative;
+            }
+        }
+
+        /// <summary>
+        /// Высчитывание ряда Тейлора в опредёлнной точке
+        /// </summary>
+        /// <param name="x">значения точки</param>
+        /// <returns>значения ряда Тейлора</returns>
+        public double Compute(double x) {
+            double res = Coefs[0];
+
+            for (int i = 1; i < N + 1; i++) {
+                double coef = Coefs[i];
+                int factorial = 1;
+                for (int j = 2; j <= i; j++) factorial *= j;
+                coef /= factorial;
+                coef *= x - X0;
+                res += coef;
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Перевод ряда Тейлора в строковый вид
+        /// </summary>
+        /// <returns>строковый вид ряда Тейлора</returns>
+        public override string ToString() {
+            string res = Coefs[0].ToString();
+
+            string x = "x";
+            if (X0 != 0) x = $"(x{(X0 > 0 ? "+" : "")}{X0})";
+
+            for (int i = 1; i < N + 1; i++) {
+                res += "";
+                double coef = Coefs[i];
+                int factorial = 1;
+                for (int j = 2; j <= i + 1; j++) factorial *= j;
+                coef /= factorial;
+                res += $"{(coef > 0 ? "+" : "")}{coef}{x}";
+            }
+
+            return res;
+        }
+    }
+}
--- FunctionSeriesConsoleApp/Program.cs
+++ FunctionSeriesConsoleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using FunctionSeriesClassLibrary;
 using System.Diagnostics;
 using System.Numerics;
+using MathNet.Symbolics;
+using Expr = MathNet.Symbolics.SymbolicExpression;
 
 //FourierSeries fs = new FourierSeries(10, 6.28, "x", FourierSeriesType.Sin);
 //Console.WriteLine(fs);
@@ -10,8 +12,40 @@ using System.Numerics;
 //sw.Stop();
 //Console.WriteLine(sw.ElapsedMilliseconds);
 
-string test = "3";
-bool test2 = Interpreter.IsVarInPolExpression(test);
-Console.WriteLine(test2);
+//string test = "3";
+//bool test2 = Interpreter.IsVarInPolExpression(test);
+//Console.WriteLine(test2);
 
-Console.WriteLine(new FourierApprox(new List<(double x, double y)>() {(1,1),(2,2)},2).Compute(1.5));
+//Console.WriteLine(new FourierApprox(new List<(double x, double y)>() {(1,1),(2,2)},2).Compute(1.5));
+
+
+//Expr x = Expr.Variable("x");
+//Expr func = new Expr(Infix.Parse("(-x-1)^2+cos((-x-1))").ResultValue);
+//Console.WriteLine("f(x)= " + func.ToString());
+
+//Expr derivative = func.Differentiate(x);
+//Console.WriteLine("f'(x)= "+derivative.ToString());
+
+//string TaylorSeries(string fun, double x0) {
+//    string res = "";
+//    string newFun = fun;
+//    if (x0 != 0) newFun.Replace("x", $"(x{(x0 > 0 ? "-" : "+")}{x0})");
+
+//    Expr x = Expr.Variable("x");
+//    Expr func = new Expr(Infix.Parse(newFun).ResultValue);
+//    Expr derivative1 = func.Differentiate(x);
+//    Expr derivative2 = derivative1.Differentiate(x);
+//    Expr derivative3 = derivative2.Differentiate(x);
+//    res += $"({derivative1})/1!*(x-{x0}) ";
+//    res += $"+ ({derivative2})/2!*(x-{x0})^2 ";
+//    res += $"+ ({derivative3})/6!*(x-{x0})^3 ";
+//    res += $"... +({func})^(n)({x0})/n!*(x-{x0})^n";
+//    res += $"+ ...";
+//    return res;
+//}
+
+//Console.WriteLine(TaylorSeries("x^2+2", 2));
+
+TaylorSeries test = new TaylorSeries("sin(x)", 0, 2);
+Console.WriteLine(test);
+Console.WriteLine(test.Compute(2));

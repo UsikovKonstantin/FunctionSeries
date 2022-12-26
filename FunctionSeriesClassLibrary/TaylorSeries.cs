b66aeea826e0741e7292d4c8cc9c19a1b9dd0d81
UsikovKonstantin <99312736+UsikovKonstantin@users.noreply.github.com>
2022-12-26 21:02:09 +0300

    Декор класса TaylorSeries

--- FunctionSeriesClassLibrary/TaylorSeries.cs
+++ FunctionSeriesClassLibrary/TaylorSeries.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MathNet.Symbolics;
+﻿using MathNet.Symbolics;
 using Expr = MathNet.Symbolics.SymbolicExpression;
 
-namespace FunctionSeriesClassLibrary {
-    public class TaylorSeries {
+namespace FunctionSeriesClassLibrary 
+{
+    public class TaylorSeries 
+    {
+        #region Поля
         private string function; // заданная функция
         private double x0; // x0
         private double[] coefs; // коэффициенты
         private int n; // кол-во коэффициентов в ряде
+        #endregion
 
+        #region Свойства
         /// <summary>
         /// Свойство "функция"
         /// </summary>
@@ -29,189 +29,79 @@ namespace FunctionSeriesClassLibrary {
         /// Свойство "кол-во коэффициентов в ряде"
         /// </summary>
         public int N { get { return n; } }
+        #endregion
 
+        #region Конструкторы
         /// <summary>
-        /// Конструктор
+        /// Конструктор.
         /// </summary>
-        /// <param name="function">заданная функция</param>
-        /// <param name="x0">x0</param>
-        /// <param name="n">кол-во коэффициентов в ряде</param>
-        /// <param name="computeCoefs">вычислять ли коэффициенты сразу же при создания объекта</param>
-        /// <exception cref="Exception">если кол-во коэффициентов в ряду меньше нуля</exception>
-        public TaylorSeries(string function, double x0, int n, bool computeCoefs = true, CancellationToken ct = new()) {
-            if (n < 0) throw new Exception("Кол-во коэффициентов в ряду должно быть больше нуля");
+        /// <param name="function"> заданная функция </param>
+        /// <param name="x0"> x0 </param>
+        /// <param name="n"> кол-во коэффициентов в ряде </param>
+        public TaylorSeries(string function, double x0, int n, CancellationToken ct = new()) 
+        {
             this.function = function;
             this.x0 = x0;
             coefs = new double[n + 1];
             this.n = n;
-            if (computeCoefs) FindCoefs(ct:ct);
+            FindCoefs(ct);
         }
 
         /// <summary>
-        /// Сложение рядов Тейлора
+        /// Конструктор по умолчанию.
         /// </summary>
-        /// <param name="t1">ряд Тейлора 1</param>
-        /// <param name="t2">ряд Тейлора 2</param>
-        /// <returns>результат сложения</returns>
-        /// <exception cref="Exception">если x0 у рядов не равны</exception>
-        public static TaylorSeries operator +(TaylorSeries t1, TaylorSeries t2) {
-            if (t1.X0 != t2.X0) throw new Exception("x0 должны быть равны");
-            int n = Math.Max(t1.N, t2.N);
-            string function = $"{t1.Function} + ${t2.Function}";
-            double[] coefs = new double[n + 1];
-            for (int i = 0; i < t1.Coefs.Length; i++) coefs[i] += t1.Coefs[i];
-            for (int i = 0; i < t2.Coefs.Length; i++) coefs[i] += t2.Coefs[i];
-            TaylorSeries res = new TaylorSeries(function, t1.X0, n, false);
-            res.coefs = coefs;
-            return res;
+        public TaylorSeries()
+        {
+            function = "sin(x)";
+            x0 = 0;
+            n = 10;
+            coefs = new double[n + 1];
+            FindCoefs();
         }
 
         /// <summary>
-        /// Переопределение унарного оператора "+"
+        /// Найти все коэфициенты ряда
         /// </summary>
-        /// <param name="ts">ряд Тейлора</param>
-        /// <returns>ряд Тейлора</returns>
-        public static TaylorSeries operator +(TaylorSeries ts) {
-            return ts;
-        }
-
-
-        /// <summary>
-        /// Вычитание рядов Тейлора
-        /// </summary>
-        /// <param name="t1">ряд Тейлора 1</param>
-        /// <param name="t2">ряд Тейлора 2</param>
-        /// <returns>результат вычитания</returns>
-        /// <exception cref="Exception">если x0 у рядов не равны</exception>
-        public static TaylorSeries operator -(TaylorSeries t1, TaylorSeries t2) {
-            if (t1.X0 != t2.X0) throw new Exception("x0 должны быть равны");
-            int n = Math.Max(t1.N, t2.N);
-            string function = $"{t1.Function} - ${t2.Function}";
-            double[] coefs = new double[n + 1];
-            for (int i = 0; i < t1.Coefs.Length; i++) coefs[i] += t1.Coefs[i];
-            for (int i = 0; i < t2.Coefs.Length; i++) coefs[i] -= t2.Coefs[i];
-            TaylorSeries res = new TaylorSeries(function, t1.X0, n, false);
-            res.coefs = coefs;
-            return res;
-        }
-
-        /// <summary>
-        /// Переопределение унарного оператора "-"
-        /// </summary>
-        /// <param name="ts">ряд Тейлора</param>
-        /// <returns>ряд Тейлора</returns>
-        public static TaylorSeries operator -(TaylorSeries ts) {
-            double[] coefs = new double[ts.N + 1];
-            for (int i = 0; i < ts.Coefs.Length; i++) coefs[i] -= ts.Coefs[i];
-            TaylorSeries res = new TaylorSeries(ts.Function, ts.X0, ts.N, false);
-            res.coefs = coefs;
-            return res;
-        }
-
-        /// <summary>
-        /// Проверка на равность рядов Тейлора
-        /// </summary>
-        /// <param name="t1">ряд Тейлора 1</param>
-        /// <param name="t2">ряд Тейлора 2</param>
-        /// <returns>Ряды тейлора равны - True, иначе - False</returns>
-        public static bool operator ==(TaylorSeries t1, TaylorSeries t2) {
-            if (t1.X0 != t2.X0) return false;
-            if (t1.N != t2.N) return false;
-            for (int i = 0; i < t1.Coefs.Length; i++) {
-                if (t1.Coefs[i] != t2.Coefs[i]) return false;
+        private void FindCoefs(CancellationToken ct = new())
+        {
+            coefs[0] = FindFirst();
+            Expr x = Expr.Variable("x");
+            Expr prevDerivative = new Expr(Infix.Parse(function).ResultValue);
+            double factorial = 1;
+            for (int i = 1; i < n + 1; i++)
+            {
+                if (ct.IsCancellationRequested) return;
+                prevDerivative = prevDerivative.Differentiate(x);
+                string pol = Interpreter.GetPolishExpression(prevDerivative.ToString(), ct);
+                coefs[i] = Interpreter.SolvePolishExpression(pol, new Dictionary<char, double>() { { 'x', x0 } });
+                factorial *= i;
+                coefs[i] /= factorial;
             }
-            return true;
         }
 
         /// <summary>
-        /// Проверка на не равность рядов Тейлора
+        /// Найти первый коэфициент ряда
         /// </summary>
-        /// <param name="t1">ряд Тейлора 1</param>
-        /// <param name="t2">ряд Тейлора 2</param>
-        /// <returns>Ряды тейлора не равны - True, иначе - False</returns>
-        public static bool operator !=(TaylorSeries t1, TaylorSeries t2) {
-            return !(t1 == t2);
-        }
-
-        /// <summary>
-        /// Умножение ряда на число
-        /// </summary>
-        /// <param name="ts">ряд Тейлора</param>
-        /// <param name="k">число</param>
-        /// <returns>результат умножения</returns>
-        public static TaylorSeries operator *(TaylorSeries ts, double k) {
-            double[] coefs = new double[ts.N + 1];
-            for (int i = 0; i < ts.Coefs.Length; i++) coefs[i] = ts.Coefs[i] * k;
-            TaylorSeries res = new TaylorSeries(ts.Function, ts.X0, ts.N, false);
-            res.coefs = coefs;
+        /// <returns></returns>
+        private double FindFirst()
+        {
+            string pol = Interpreter.GetPolishExpression(function);
+            double res = Interpreter.SolvePolishExpression(pol, new Dictionary<char, double>() { { 'x', x0 } });
             return res;
         }
+        #endregion
 
-        /// <summary>
-        /// Умножение ряда на число
-        /// </summary>
-        /// <param name="k">число</param>
-        /// <param name="ts">ряд Тейлора</param>
-        /// <returns>результат умножения</returns>
-        public static TaylorSeries operator *(double k, TaylorSeries ts) {
-            return ts * k;
-        }
-
-        /// <summary>
-        /// Дифференцировать ряд Тейлора
-        /// </summary>
-        /// <returns>производная исходного ряда Тейлора</returns>
-        public TaylorSeries GetDerivative() {
-            double[] newCoefs = new double[n + 1];
-            for (int i = 1; i < n + 1; i++) {
-                newCoefs[i - 1] = coefs[i] * i;
-            }
-            TaylorSeries newTaylorSeries;
-            if (n != 0) newTaylorSeries = new TaylorSeries($"d({function})/dx", x0, n, false);
-            else newTaylorSeries = new TaylorSeries($"d({function})/dx", x0, n, false);
-            newTaylorSeries.coefs = newCoefs;
-            return newTaylorSeries;
-        }
-
-        /// <summary>
-        /// Проинтегрировать ряд Тейлора
-        /// </summary>
-        /// <returns>проинтегрированный исходный ряд Тейлора</returns>
-        public TaylorSeries GetIntegral() {
-            double[] newCoefs = new double[n + 2];
-            for (int i = 0; i < n + 1; i++) {
-                newCoefs[i + 1] = coefs[i] / (i + 1);
-            }
-            TaylorSeries newTaylorSeries = new TaylorSeries($"∫({function})dx", x0, n, false);
-            newTaylorSeries.coefs = newCoefs;
-            return newTaylorSeries;
-        }
-
-        /// <summary>
-        /// Высчитывание ряда Тейлора в опредёлнной точке
-        /// </summary>
-        /// <param name="x">значения точки</param>
-        /// <returns>значения ряда Тейлора</returns>
-        public double Compute(double x) {
-            double res = coefs[0];
-
-            for (int i = 1; i < n + 1; i++) {
-                double coef = coefs[i];
-                coef *= Math.Pow(x - x0, i);
-                res += coef;
-            }
-
-            return res;
-        }
-
+        #region Переопределение ToString()
         /// <summary>
         /// Перевод ряда Тейлора в строковый вид
         /// </summary>
         /// <returns>строковый вид ряда Тейлора</returns>
-        public override string ToString() {
+        public override string ToString()
+        {
             string res = coefs[0].ToString();
             bool hasFirst = true;
-            if (coefs[0] == 0) {
+            if (coefs[0] == 0)
+            {
                 res = "";
                 hasFirst = false;
             }
@@ -219,10 +109,12 @@ namespace FunctionSeriesClassLibrary {
             string x = "x";
             if (x0 != 0) x = $"(x{(x0 >= 0 ? "-" : "+")}{Math.Abs(x0)})";
 
-            for (int i = 1; i < n + 1; i++) {
+            for (int i = 1; i < n + 1; i++)
+            {
                 double coef = coefs[i];
                 if (coef == 0) continue;
-                if (!hasFirst) {
+                if (!hasFirst)
+                {
                     res += $"{(coef >= 0 ? "" : "-")}{(Math.Abs(coef) == 1 ? "" : $"{Math.Abs(coef)}*")}{x}{(i > 1 ? $"^{i}" : "")}";
                     hasFirst = true;
                     continue;
@@ -232,14 +124,185 @@ namespace FunctionSeriesClassLibrary {
 
             return res;
         }
+        #endregion
 
-        // <summary>
-        /// Проверка на равность рядов Тейлора
+        #region Методы
+        /// <summary>
+        /// Высчитывание ряда Тейлора в определенной точке
         /// </summary>
-        /// <param name="t1">ряд Тейлора 1</param>
-        /// <param name="t2">ряд Тейлора 2</param>
-        /// <returns>Ряды тейлора равны - True, иначе - False</returns>
-        public override bool Equals(object? obj) {
+        /// <param name="x"> значения точки </param>
+        /// <returns> значения ряда Тейлора </returns>
+        public double Compute(double x)
+        {
+            double res = coefs[0];
+
+            double m = x - x0;
+            for (int i = 1; i < n + 1; i++)
+            {
+                res += coefs[i] * m;
+                m *= x - x0;
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// Дифференцировать ряд Тейлора
+        /// </summary>
+        /// <returns>производная исходного ряда Тейлора</returns>
+        public TaylorSeries GetDerivative()
+        {
+            double[] newCoefs = new double[n + 1];
+            for (int i = 1; i < n + 1; i++)
+            {
+                newCoefs[i - 1] = coefs[i] * i;
+            }
+            TaylorSeries newTaylorSeries = new TaylorSeries($"d({function})/dx", x0, n);
+            newTaylorSeries.coefs = newCoefs;
+            return newTaylorSeries;
+        }
+
+        /// <summary>
+        /// Проинтегрировать ряд Тейлора
+        /// </summary>
+        /// <returns>проинтегрированный исходный ряд Тейлора</returns>
+        public TaylorSeries GetIntegral()
+        {
+            double[] newCoefs = new double[n + 2];
+            for (int i = 0; i < n + 1; i++)
+            {
+                newCoefs[i + 1] = coefs[i] / (i + 1);
+            }
+            TaylorSeries newTaylorSeries = new TaylorSeries($"∫({function})dx", x0, n);
+            newTaylorSeries.coefs = newCoefs;
+            return newTaylorSeries;
+        }
+        #endregion
+
+        #region Перегрузка арифметических операторов
+        /// <summary>
+        /// Сложение рядов Тейлора
+        /// </summary>
+        /// <param name="t1"> ряд Тейлора 1 </param>
+        /// <param name="t2"> ряд Тейлора 2 </param>
+        /// <returns> результат сложения </returns>
+        public static TaylorSeries operator +(TaylorSeries t1, TaylorSeries t2) 
+        {
+            int n = Math.Max(t1.N, t2.N);
+            string function = $"{t1.Function} + ${t2.Function}";
+            double[] coefs = new double[n + 1];
+            for (int i = 0; i < t1.Coefs.Length; i++) coefs[i] += t1.Coefs[i];
+            for (int i = 0; i < t2.Coefs.Length; i++) coefs[i] += t2.Coefs[i];
+            TaylorSeries res = new TaylorSeries(function, t1.X0, n);
+            res.coefs = coefs;
+            return res;
+        }
+
+        /// <summary>
+        /// Переопределение унарного оператора "+"
+        /// </summary>
+        /// <param name="ts"> ряд Тейлора </param>
+        /// <returns> ряд Тейлора </returns>
+        public static TaylorSeries operator +(TaylorSeries ts) 
+        {
+            return ts;
+        }
+
+        /// <summary>
+        /// Вычитание рядов Тейлора
+        /// </summary>
+        /// <param name="t1"> ряд Тейлора 1 </param>
+        /// <param name="t2"> ряд Тейлора 2 </param>
+        /// <returns> результат вычитания </returns>
+        public static TaylorSeries operator -(TaylorSeries t1, TaylorSeries t2) 
+        {
+            int n = Math.Max(t1.N, t2.N);
+            string function = $"{t1.Function} - ${t2.Function}";
+            double[] coefs = new double[n + 1];
+            for (int i = 0; i < t1.Coefs.Length; i++) coefs[i] += t1.Coefs[i];
+            for (int i = 0; i < t2.Coefs.Length; i++) coefs[i] -= t2.Coefs[i];
+            TaylorSeries res = new TaylorSeries(function, t1.X0, n);
+            res.coefs = coefs;
+            return res;
+        }
+
+        /// <summary>
+        /// Переопределение унарного оператора "-"
+        /// </summary>
+        /// <param name="ts">ряд Тейлора</param>
+        /// <returns>ряд Тейлора</returns>
+        public static TaylorSeries operator -(TaylorSeries ts) 
+        {
+            double[] coefs = new double[ts.N + 1];
+            for (int i = 0; i < ts.Coefs.Length; i++) coefs[i] -= ts.Coefs[i];
+            TaylorSeries res = new TaylorSeries(ts.Function, ts.X0, ts.N);
+            res.coefs = coefs;
+            return res;
+        }
+
+        /// <summary>
+        /// Умножение ряда на число
+        /// </summary>
+        /// <param name="ts"> ряд Тейлора </param>
+        /// <param name="k"> число </param>
+        /// <returns> результат умножения </returns>
+        public static TaylorSeries operator *(TaylorSeries ts, double k)
+        {
+            double[] coefs = new double[ts.N + 1];
+            for (int i = 0; i < ts.Coefs.Length; i++) coefs[i] = ts.Coefs[i] * k;
+            TaylorSeries res = new TaylorSeries(ts.Function, ts.X0, ts.N);
+            res.coefs = coefs;
+            return res;
+        }
+
+        /// <summary>
+        /// Умножение ряда на число
+        /// </summary>
+        /// <param name="k"> число </param>
+        /// <param name="ts"> ряд Тейлора </param>
+        /// <returns> результат умножения </returns>
+        public static TaylorSeries operator *(double k, TaylorSeries ts)
+        {
+            return ts * k;
+        }
+        #endregion
+
+        #region Перегрузка операторов сравнения
+        /// <summary>
+        /// Проверка на равенство рядов Тейлора
+        /// </summary>
+        /// <param name="t1"> ряд Тейлора 1 </param>
+        /// <param name="t2"> ряд Тейлора 2 </param>
+        /// <returns> Ряды тейлора равны - True, иначе - False </returns>
+        public static bool operator ==(TaylorSeries t1, TaylorSeries t2) 
+        {
+            if (t1.X0 != t2.X0) return false;
+            if (t1.N != t2.N) return false;
+            for (int i = 0; i < t1.Coefs.Length; i++) 
+            {
+                if (t1.Coefs[i] != t2.Coefs[i]) return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Проверка на не равенство рядов Тейлора
+        /// </summary>
+        /// <param name="t1"> ряд Тейлора 1 </param>
+        /// <param name="t2"> ряд Тейлора 2 </param>
+        /// <returns> Ряды тейлора не равны - True, иначе - False </returns>
+        public static bool operator !=(TaylorSeries t1, TaylorSeries t2) 
+        {
+            return !(t1 == t2);
+        }
+
+        /// <summary>
+        /// Проверка на равенство рядов Тейлора
+        /// </summary>
+        /// <param name="t1"> ряд Тейлора 1 </param>
+        /// <param name="t2"> ряд Тейлора 2 </param>
+        /// <returns> Ряды тейлора равны - True, иначе - False </returns>
+        public override bool Equals(object? obj)
+        {
             if (obj is TaylorSeries fs)
                 return this == fs;
             else
@@ -250,38 +313,10 @@ namespace FunctionSeriesClassLibrary {
         /// Переопределение метода GetHashCode().
         /// </summary>
         /// <returns> хэш-код </returns>
-        public override int GetHashCode() {
+        public override int GetHashCode()
+        {
             return HashCode.Combine(x0, coefs, n);
         }
-
-        /// <summary>
-        /// Найти первый коэфициент ряда
-        /// </summary>
-        /// <returns></returns>
-        private double FindFirst() {
-            string pol = Interpreter.GetPolishExpression(function);
-            double res = Interpreter.SolvePolishExpression(pol, new Dictionary<char, double>() { { 'x', x0 } });
-            return res;
-        }
-
-        /// <summary>
-        /// Найти все коэфициенты ряда
-        /// </summary>
-        private void FindCoefs(CancellationToken ct = new()) {
-            coefs[0] = FindFirst();
-
-            Expr x = Expr.Variable("x");
-            Expr prevDerivative = new Expr(Infix.Parse(function).ResultValue);
-            for (int i = 1; i < n + 1; i++) {
-                if (ct.IsCancellationRequested) return;
-                Expr derivative = prevDerivative.Differentiate(x);
-                string pol = Interpreter.GetPolishExpression(derivative.ToString(), ct: ct);
-                coefs[i] = Interpreter.SolvePolishExpression(pol, new Dictionary<char, double>() { { 'x', x0 } });
-                double factorial = 1;
-                for (int j = 2; j <= i; j++) factorial *= j;
-                coefs[i] /= factorial;
-                prevDerivative = derivative;
-            }
-        }
+        #endregion 
     }
 }

7f8e8a2187305cd11364d8cffb1482b19d2be58e
theponylover666 <dotaonelove332@gmail.com>
2022-12-26 23:23:39 +0300

    Добавил тесты

--- FunctionSeriesTest/UnitTest1.cs
+++ FunctionSeriesTest/UnitTest1.cs
@@ -34,12 +34,11 @@ namespace FunctionSeriesTest
         [TestMethod]
         public void SolvePolishExpressionTest2()
         {
-            x = 1;
-            test = "(18*sin(2*x)) + 11*x^3 / (11 * cos(3*x)) + 13*x";
-            Assert.AreEqual(Interpreter.SolvePolishExpression(Interpreter.GetPolishExpression(test), new Dictionary<char, double> { { 'x', x } }), 2, 798102101837749);
+            x = 3;
+            test = "18*sin(2*x) + 11*x^3 / 11 * cos(3*x) + 13*x";
+            Assert.AreEqual(Math.Round(Interpreter.SolvePolishExpression(Interpreter.GetPolishExpression(test), new Dictionary<char, double> { { 'x', x } }), 3), 9.37);
         }
     }
-
     [TestClass]
     public class FourierSeriesTest
     {
@@ -53,15 +52,25 @@ namespace FunctionSeriesTest
         [TestMethod]
         public void FourierSer2Test()
         {
-            var test = new FourierSeries(1, 1, "x + 1", FourierSeriesType.Sin);
-            Assert.AreEqual(test.ToString(), "1,591516705860902*sin(6,283185307179586*x)");
+            var test = new FourierSeries(4, 3, "x + 1", FourierSeriesType.Sin);
+            double[] n = new double[5] { 0, 2.228, -0.477, 0.743, -0.239 };
+            for (int i = 0; i <= test.N; i++)
+            {
+                Assert.AreEqual(Math.Round(test.A[i], 3), 0);
+                Assert.AreEqual(Math.Round(test.B[i], 3), n[i]);
+            }
         }
 
         [TestMethod]
         public void FourierSer2Test2()
         {
             var test = new FourierSeries(2, 10, "x * 2", FourierSeriesType.Cos);
-            Assert.AreEqual(test.ToString(), "5 - 4,052930680054939*cos(0,6283185307179586*x) + 1,7585932710062481E-15*cos(1,2566370614359172*x)");
+            double[] n = new double[3] { 10, -4.053, 0 };
+            for (int i = 0; i <= test.N; i++)
+            {
+                Assert.AreEqual(Math.Round(test.A[i], 3), n[i]);
+                Assert.AreEqual(Math.Round(test.B[i], 3), 0);
+            }
         }
 
         [TestMethod]
@@ -89,42 +98,42 @@ namespace FunctionSeriesTest
         public void ComputeTest()
         {
             var test = new FourierSeries(new double[] { 0.0, 1.0 }, new double[] { 0.0, 1.0 }, 2 * Math.PI, FourierSeriesType.CosSin);
-            Assert.AreEqual(test.Compute(0.1).ToString(), "1,094837581924854");
+            Assert.AreEqual(Math.Round(test.Compute(0.1), 3), 1.095);
         }
 
         [TestMethod]
         public void ComputeTest2()
         {
             var test = new FourierSeries(new double[] { 0.0, 1.0 }, new double[] { 0.0, 1.0 }, 2 * Math.PI, FourierSeriesType.CosSin);
-            Assert.AreEqual(test.Compute(15).ToString(), "-0,10940007270170449");
+            Assert.AreEqual(Math.Round(test.Compute(15), 3), -0.109);
         }
 
         [TestMethod]
         public void ComputeTest3()
         {
             var test = new FourierSeries(new double[] { 10, 15 }, new double[] { 15, 20 }, Math.PI, FourierSeriesType.Cos);
-            Assert.AreEqual(test.Compute(1).ToString(), "16,9437459883065");
+            Assert.AreEqual(Math.Round(test.Compute(1), 3), 16.944);
         }
 
         [TestMethod]
         public void IntegralTest()
         {
             var test = new FourierSeries(new double[] { 0.0, 1.0 }, new double[] { 0.0, 1.0 }, 2 * Math.PI, FourierSeriesType.CosSin);
-            Assert.AreEqual(test.Integral("e x ^", -3.14, 3.14, 50000).ToString(), "23,06058409113614");
+            Assert.AreEqual(Math.Round(test.Integral("e x ^", -3.14, 3.14, 50000), 3), 23.061);
         }
 
         [TestMethod]
         public void IntegralTest2()
         {
             var test = new FourierSeries(new double[] { 0.0, 1.0 }, new double[] { 0.0, 1.0 }, 2 * Math.PI, FourierSeriesType.CosSin);
-            Assert.AreEqual(test.Integral("e", -3.14, 3.14, 50000).ToString(), "17,070809882730966");
+            Assert.AreEqual(Math.Round(test.Integral("e", -3.14, 3.14, 50000), 3), 17.071);
         }
 
         [TestMethod]
         public void IntegralTest3()
         {
             var test = new FourierSeries(new double[] { 10, 15 }, new double[] { 15, 20 }, Math.PI, FourierSeriesType.Cos);
-            Assert.AreEqual(test.Integral("e 2 ^", -3.14, 3.14, 100000).ToString(), "46,40327230121512");
+            Assert.AreEqual(Math.Round(test.Integral("e 2 ^", -3.14, 3.14, 100000), 3), 46.403);
         }
 
         [TestMethod]
@@ -291,7 +300,246 @@ namespace FunctionSeriesTest
             Assert.AreEqual(test2.ToString(), "15 + 45*cos(2*x) + 60*sin(2*x)");
         }
     }
+    [TestClass]
+    public class TaylorSeriesTest
+    {
+        [TestMethod]
+        public void TaylorSerTest()
+        {
+            var test = new TaylorSeries("e^x", 0, 2);
+            Assert.AreEqual(test.ToString(), "1 + x + 0,5*x^2");
+        }
 
+        [TestMethod]
+        public void TaylorSer2Test()
+        {
+            var test = new TaylorSeries("cos(x) + sin(x)", 0, 2);
+            Assert.AreEqual(test.ToString(), "1 + x - 0,5*x^2");
+        }
+
+        [TestMethod]
+        public void TaylorSer3Test()
+        {
+            var test = new TaylorSeries("cos(x) * e^x", 0, 2);
+            Assert.AreEqual(test.ToString(), "1 + x");
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            var test = new TaylorSeries("e^x", 0, 3);
+            var test2 = new TaylorSeries("sin(x)", 0, 3);
+            var test3 = test + test2;
+            Assert.AreEqual(test3.ToString(), "1 + 2*x + 0,5*x^2");
+        }
+
+        [TestMethod]
+        public void AddTest2()
+        {
+            var test = new TaylorSeries("cos(x) + sin(x)", 0, 2);
+            var test2 = new TaylorSeries("sin(x)", 0, 3);
+            var test3 = test + test2;
+            Assert.AreEqual(test3.ToString(), "1 + 2*x - 0,5*x^2 - 0,16666666666666666*x^3");
+        }
+
+        [TestMethod]
+        public void AddTest3()
+        {
+            var test = new TaylorSeries("cos(x)", 0, 2);
+            var test2 = new TaylorSeries("sin(x)", 0, 2);
+            var test3 = test + test2;
+            Assert.AreEqual(test3.ToString(), "1 + x - 0,5*x^2");
+        }
+
+        [TestMethod]
+        public void ComputeTest()
+        {
+            var test = new TaylorSeries("e^x", 1, 100);
+            Assert.AreEqual(Math.Round(test.Compute(3), 3), 20.086);
+        }
+
+        [TestMethod]
+        public void ComputeTest2()
+        {
+            var test = new TaylorSeries("cos(x)*sin(x)", 1, 100);
+            Assert.AreEqual(Math.Round(test.Compute(5), 3), -0.272);
+        }
+
+        [TestMethod]
+        public void ComputeTest3()
+        {
+            var test = new TaylorSeries("cos(x)", 1, 100);
+            Assert.AreEqual(Math.Round(test.Compute(1), 3), 0.54);
+        }
+
+        [TestMethod]
+        public void UnaryMinusTest()
+        {
+            var test = new TaylorSeries("e^x", 0, 2);
+            var test2 = -test;
+            Assert.AreEqual(test2.ToString(), "-1 - x - 0,5*x^2");
+        }
+
+        [TestMethod]
+        public void UnaryMinus2Test()
+        {
+            var test = new TaylorSeries("cos(x) + sin(x)", 0, 2);
+            var test2 = -test;
+            Assert.AreEqual(test2.ToString(), "-1 - x + 0,5*x^2");
+        }
+
+        [TestMethod]
+        public void UnaryMinus3Test()
+        {
+            var test = new TaylorSeries("cos(x) * e^x", 0, 2);
+            var test2 = -test;
+            Assert.AreEqual(test2.ToString(), "-1 - x");
+        }
+
+        [TestMethod]
+        public void CompTest()
+        {
+            var test = new TaylorSeries("e^x", 0, 3);
+            var test2 = new TaylorSeries("sin(x)", 0, 3);
+            bool test3 = test == test2;
+            Assert.AreEqual(test3, false);
+        }
+
+        [TestMethod]
+        public void CompTest2()
+        {
+            var test = new TaylorSeries("sin(x)", 0, 2);
+            var test2 = new TaylorSeries("cos(x) * sin(x)", 0, 2);
+            bool test3 = test == test2;
+            Assert.AreEqual(test3, true);
+        }
+
+        [TestMethod]
+        public void CompTest3()
+        {
+            var test = new TaylorSeries("cos(x)", 0, 2);
+            var test2 = new TaylorSeries("sin(x)", 0, 2);
+            bool test3 = test == test2;
+            Assert.AreEqual(test3, false);
+        }
+
+        [TestMethod]
+        public void UnCompTest()
+        {
+            var test = new TaylorSeries("e^x", 0, 3);
+            var test2 = new TaylorSeries("sin(x)", 0, 3);
+            bool test3 = test != test2;
+            Assert.AreEqual(test3, true);
+        }
+
+        [TestMethod]
+        public void UnCompTest2()
+        {
+            var test = new TaylorSeries("sin(x)", 0, 2);
+            var test2 = new TaylorSeries("cos(x) * sin(x)", 0, 2);
+            bool test3 = test != test2;
+            Assert.AreEqual(test3, false);
+        }
+
+        [TestMethod]
+        public void UnCompTest3()
+        {
+            var test = new TaylorSeries("cos(x)", 0, 2);
+            var test2 = new TaylorSeries("sin(x)", 0, 2);
+            bool test3 = test != test2;
+            Assert.AreEqual(test3, true);
+        }
+
+        [TestMethod]
+        public void MulTest()
+        {
+            var test = new TaylorSeries("e^x", 0, 2);
+            var test2 = test * 5;
+            Assert.AreEqual(test2.ToString(), "5 + 5*x + 2,5*x^2");
+        }
+
+        [TestMethod]
+        public void MulTest2()
+        {
+            var test = new TaylorSeries("cos(x) + sin(x)", 0, 2);
+            var test2 = test * 10;
+            Assert.AreEqual(test2.ToString(), "10 + 10*x - 5*x^2");
+        }
+
+        [TestMethod]
+        public void MulTest3()
+        {
+            var test = new TaylorSeries("cos(x) * e^x", 0, 2);
+            var test2 = test * 3;
+            Assert.AreEqual(test2.ToString(), "3 + 3*x");
+        }
+
+        [TestMethod]
+        public void Mul2Test()
+        {
+            var test = new TaylorSeries("e^x", 0, 2);
+            var test2 = 5 * test;
+            Assert.AreEqual(test2.ToString(), "5 + 5*x + 2,5*x^2");
+        }
+
+        [TestMethod]
+        public void Mul2Test2()
+        {
+            var test = new TaylorSeries("cos(x) + sin(x)", 0, 2);
+            var test2 = 10 * test;
+            Assert.AreEqual(test2.ToString(), "10 + 10*x - 5*x^2");
+        }
+
+        [TestMethod]
+        public void Mul2Test3()
+        {
+            var test = new TaylorSeries("cos(x) * e^x", 0, 2);
+            var test2 = 3 * test;
+            Assert.AreEqual(test2.ToString(), "3 + 3*x");
+        }
+
+        [TestMethod]
+        public void GetDerivativeTest()
+        {
+            var test = new TaylorSeries("e^x", 0, 2);
+            Assert.AreEqual(test.GetDerivative().ToString(), "1 + x");
+        }
+
+        [TestMethod]
+        public void GetDerivativeTest2()
+        {
+            var test = new TaylorSeries("cos(x) + sin(x)", 0, 3);
+            Assert.AreEqual(test.GetDerivative().ToString(), "1 - x - 0,5*x^2");
+        }
+
+        [TestMethod]
+        public void GetDerivativeTest3()
+        {
+            var test = new TaylorSeries("cos(x) * e^x", 0, 2);
+            Assert.AreEqual(test.GetDerivative().ToString(), "1");
+        }
+
+        [TestMethod]
+        public void GetIntegralTest()
+        {
+            var test = new TaylorSeries("e^x", 0, 2);
+            Assert.AreEqual(test.GetIntegral().ToString(), "x + 0,5*x^2");
+        }
+
+        [TestMethod]
+        public void GetIntegralTest2()
+        {
+            var test = new TaylorSeries("cos(x) + sin(x)", 0, 3);
+            Assert.AreEqual(test.GetIntegral().ToString(), "x + 0,5*x^2 - 0,16666666666666666*x^3");
+        }
+
+        [TestMethod]
+        public void GetIntegralTest3()
+        {
+            var test = new TaylorSeries("cos(x) * e^x", 0, 2);
+            Assert.AreEqual(test.GetIntegral().ToString(), "x + 0,5*x^2");
+        }
+    }
     [TestClass]
     public class AproxximationTest
     {
